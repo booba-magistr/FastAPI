fc1f681b173690f6dc97327369b163a042941cff
booba-magistr <mainpo4ta1337@mail.ru>
2024-12-03 21:43:16 +0300

    add first api

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
+from api import router as api_router
+
+
+app = FastAPI()
+app.include_router(api_router, prefix='/api')
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', reload=True)
