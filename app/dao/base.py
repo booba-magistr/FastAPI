8499b9ed4a4847f7da2a0d6d2cac93f1dbd5aabb
booba-magistr <mainpo4ta1337@mail.ru>
2024-12-18 13:51:38 +0300

    now i can get one product using product_id

--- app/dao/base.py
+++ app/dao/base.py
@@ -12,3 +12,11 @@ class BaseDAO:
             products = await session.execute(query)
             result = products.scalars().all()
             return result
+        
+    @classmethod
+    async def get_one_or_nothing(cls, product_id:int):
+        async with session_maker() as session:
+            query = select(cls.model).filter_by(id=product_id)
+            product = await session.execute(query)
+            result = product.scalar_one_or_none()
+            return result
--- app/products/router.py
+++ app/products/router.py
@@ -7,7 +7,15 @@ from products.rb import RBProduct
 product_router = APIRouter(prefix='/product', tags=['Товары'])
 
 
-@product_router.get('/', summary='Получить меню', response_model=list[Product])
-async def product(request_body: RBProduct = Depends()):
+@product_router.get('/', summary='Get product menu', response_model=list[Product])
+async def get_products(request_body: RBProduct = Depends()):
     # response_model- example value response body
     return await ProductDAO.get_all(**request_body.to_dict())
+
+
+@product_router.get('/{product_id}', summary='Get product using id')
+async def get_product(product_id:int):
+    result = await ProductDAO.get_one_or_nothing(product_id)
+    if result is None:
+        return {'message': f'Товар с id:{product_id} не найден'}
+    return result
