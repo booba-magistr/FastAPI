bf582d12665b1e78c49ea0b5df5ca30fe44f5a69
booba-magistr <mainpo4ta1337@mail.ru>
2024-12-31 20:28:55 +0500

    add attribute count in Category model

--- app/category/models.py
+++ app/category/models.py
@@ -1,10 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String
+from sqlalchemy import String, text
 from database import Base
 
 
 class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(150))
+    # server_default- значение на уровне БД, default- на уровне объектов и моделей в Python коде
+    # В таком случае значение бы 0 подставлялось в таблицу при добавлении записи, но в самой таблице
+    # не отобразилось данная информация
+    count: Mapped[int] = mapped_column(server_default=text('0'))
 
     products: Mapped[list['Product']] = relationship('Product', back_populates='category')
--- app/category/router.py
+++ app/category/router.py
@@ -6,7 +6,7 @@ from .schemas import SchemaCategoryAdd, SchemaCategoryUpdate
 category_router = APIRouter(prefix='/category', tags=['Категории'])
 
 
-@category_router.post('/add/')
+@category_router.post('/add/', summary='Add category')
 async def add_category(category: SchemaCategoryAdd):
     status = await CategoryDAO.add(**category.dict())
 
@@ -14,7 +14,7 @@ async def add_category(category: SchemaCategoryAdd):
         return {'status': 'success', 'category': status}
     return {'status': 'add error'}
 
-@category_router.put('/update/')
+@category_router.put('/update/', summary='Update category')
 async def update_category(category: SchemaCategoryUpdate):
     status = await CategoryDAO.update(filter_by={'id': category.id},
                                       name=category.name)
@@ -23,7 +23,7 @@ async def update_category(category: SchemaCategoryUpdate):
         return {'status': 'success', 'category': category}
     return {'status': 'update error'}
 
-@category_router.delete('/delete/{category_id}')  # Без модели, тк тут всего один параметр
+@category_router.delete('/delete/{category_id}', summary='Delete category')  # Без модели, тк тут всего один параметр
 async def delete(category_id: int):
     status = await CategoryDAO.delete(id=category_id)
 
