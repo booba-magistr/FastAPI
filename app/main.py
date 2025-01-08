df16dbc868a48931d3a3621574b2d9b08420dba2
booba-magistr <mainpo4ta1337@mail.ru>
2025-01-08 21:53:24 +0500

    connect static files in main.py add css styles

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from products.router import product_router
 from category.router import category_router
 from users.router import user_router
 from pages.router import front_router
-
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -18,6 +18,9 @@ app.include_router(product_router)
 app.include_router(category_router)
 app.include_router(user_router)
 app.include_router(front_router)
+# настройка приложение FastAPI для обслуживания стат. файлов 
+# '/static'- путь по которому будут доступны стат. файлы(http://localhost:8000/static/test.png) 
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
 
 
 if __name__ == '__main__':
