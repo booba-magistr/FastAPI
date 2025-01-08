34788d24b776c3022068717d367ce5854b16ec45
booba-magistr <mainpo4ta1337@mail.ru>
2025-01-08 17:03:19 +0500

    add depend get_products_html from get_products handler and updated html file

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from products.router import get_products
 
 
 front_router = APIRouter(prefix='/pages', tags=['Фронт'])
 templates = Jinja2Templates(directory='app/templates')
 
 @front_router.get('/products')  # localhost:8000/pages/products
-async def get_products_html(request: Request):  # Обязательный параметр request для управления
+async def get_products_html(request: Request, products=Depends(get_products)):  # Обязательный параметр request для управления
     # различными функциями (url-генерация и работа с сессиями)
-    return templates.TemplateResponse(name='index.html', context={'request': request})
+    return templates.TemplateResponse(name='index.html', context={'request': request,
+                                                                  'products': products})
