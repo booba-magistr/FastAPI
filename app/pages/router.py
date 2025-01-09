1ccf41bd6747b7ffa6e6b818d23f6b7480b7f3c1
booba-magistr <mainpo4ta1337@mail.ru>
2025-01-09 22:19:30 +0500

    add handler for html page of product and css styles

--- app/pages/router.py
+++ app/pages/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Depends, UploadFile
 import shutil
 from fastapi.templating import Jinja2Templates
-from products.router import get_products
+from products.router import get_products, get_product
 
 
 front_router = APIRouter(prefix='/pages', tags=['Фронт'])
@@ -18,3 +18,9 @@ async def add_photo(file: UploadFile, img_name: int):
     # webp- сужение фото без потери качества
     with open(f'app/static/images/{img_name}.webp', 'wb+') as photo_obj:
         shutil.copyfileobj(file.file, photo_obj)
+
+
+@front_router.get('/products/{product_id}')
+async def get_product_html(request: Request, product = Depends(get_product)):
+    return templates.TemplateResponse(name='product.html', context={'request': request,
+                                                                    'product': product})
